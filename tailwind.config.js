61558febe00fde3a165c97407c0309f9521b6397
Anish Singh <anishsingh02@hotmail.com>
2023-07-04 16:05:20 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "https://github.com/anishhhsingh/instrumus-app/blob/main/index.html",
+    "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
