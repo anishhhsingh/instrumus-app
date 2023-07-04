975b4ac2599861ef5964f5e79953f41458f351ce
Anish Singh <anishsingh02@hotmail.com>
2023-07-04 16:07:29 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from 'https://github.com/anishhhsingh/instrumus-app/blob/main/src/App.jsx'
-import 'https://github.com/anishhhsingh/instrumus-app/blob/main/src/index.css'
+import App from './App.jsx'
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
